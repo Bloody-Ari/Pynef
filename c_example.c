4ef71156ee60eed696887e6e3055c1b479d691a9
Ari <alicewulfric@gmail.com>
2026-05-12 20:54:32 -0600

    m

--- c_example.c
+++ c_example.c
@@ -14,11 +14,19 @@ int main(){
   float new_nozzle[] = {8.0, 5.0, 2.0, 6.0, 7.0};
   char file_name[] = {"EwEwE"};
   char file_type[] = {"3mf"};
-  // char target_dir[] = {"/home/eliz/Downloads"};
+  char target_dir[] = {"/tmp/"};
 
+  // default directory usage
   if(pynefWrapper(new_nozzle, file_name, file_type, NULL) == 0){
-    printf("[FROM C] All good!");
-    return 0;
+    printf("[FROM C] All good using default directory!");
+  } else {
+    printf("[FROM C] Something failed...");
+    return -1;
+  }
+
+  // using custom directory
+  if(pynefWrapper(new_nozzle, file_name, file_type, target_dir) == 0){
+    printf("[FROM C] All good using custom directory!");
   } else {
     printf("[FROM C] Something failed...");
     return -1;
